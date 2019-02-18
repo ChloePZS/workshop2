ef87053af1b4fd8e4390fd8022d5d56e5349ffcf
ChloePZS <chloe-pozas@hotmail.fr>
2019-02-18 13:30:51 +0100

    test

--- Explore_data.R
+++ Explore_data.R
@@ -1,2 +1,22 @@
+1_Explore_data
+
 library(tidyverse)
 data<-read.csv("data/fish_lw.csv")
+
+summary(data)
+
+
+
+
+#remove missing values
+data<-data[!is.na(data$weight),]
+
+#Visualize
+plot<-
+  ggplot(data)+
+  geom_point(aes(log(tl), log(wieght)))+
+  geom_abline(slope)
+
+ggsave("", plot)
+
+
